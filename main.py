df84776ba44e8b4293d2b26c07e766a4bb365211
0starzyk <starzyk@student.agh.edu.pl>
2023-04-04 21:37:02 +0200

    Red detection

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    img = cv2.imread("tree.jpg", cv2.IMREAD_GRAYSCALE)
-    cv2.imshow("Window 1", img)
-    lower = 0
-    higher = 3
-    new = cv2.inRange(img, lower, higher)
-    cv2.imshow("Window2 ", new)
+    original_image = cv2.imread("traffic_light.jpg", cv2.IMREAD_COLOR)
+    cv2.imshow("Window 1", original_image)
+    # 255, 137, 144 --- 97, 29, 40
+    lower_threshold = np.array([40, 29, 120])
+    higher_threshold = np.array([144, 137, 255])
+    thresholded_image = cv2.inRange(original_image, lower_threshold, higher_threshold)
+    cv2.imshow("Window 2", thresholded_image)
+
+    kernel = np.ones((5, 5))
+    eroded_image = cv2.erode(thresholded_image, kernel, iterations=1)
+    # eroded_image = cv2.dilate(eroded_image, kernel, iterations=1)
+
+    cv2.imshow("Window 3", eroded_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
