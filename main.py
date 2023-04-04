6380cbe931eb50d339c572d8985cc847c1b5f42e
0starzyk <starzyk@student.agh.edu.pl>
2023-04-04 17:47:16 +0200

    Binarize image

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def click_event(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, ' ', y)
-
-
-if __name__ == '__main__':
-    img = cv2.imread(cv2.samples.findFile("tree.jpg"))
-    cv2.imshow("Window", img)
-    cv2.setMouseCallback('Window', click_event)
+if __name__ == "__main__":
+    img = cv2.imread("tree.jpg", cv2.IMREAD_GRAYSCALE)
+    cv2.imshow("Window 1", img)
+    lower = 0
+    higher = 3
+    new = cv2.inRange(img, lower, higher)
+    cv2.imshow("Window2 ", new)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
