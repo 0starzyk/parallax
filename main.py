136e693336f2f61711a350c94d38f30bbeb6d7bf
0starzyk <starzyk@student.agh.edu.pl>
2023-04-05 03:34:20 +0200

    Dilation of output image

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ if __name__ == "__main__":
         value - config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
     higher_threshold = np.array(list(map(lambda value: utils.clamp(
         value + config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
-    
     threshold_image = cv2.inRange(original_image, lower_threshold, higher_threshold)
-    cv2.imshow("Window 2", thresholded_image)
+
+    kernel = np.ones((5, 5))
+    dilated_image = cv2.dilate(threshold_image, kernel, iterations=1)
+
+    cv2.imshow("Window 2", dilated_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
