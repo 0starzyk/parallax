6590518ebb2b23caf8a014365e4072ebd28f0c01
0starzyk <starzyk@student.agh.edu.pl>
2023-04-05 03:26:12 +0200

    Clamping values and creating config file

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+import config
 from MouseDetector import MouseDetector
+import utils
 
 
 if __name__ == "__main__":
-    original_image = cv2.imread("tree_modified.jpg", cv2.IMREAD_COLOR)
+    original_image = cv2.imread("traffic_light.jpg", cv2.IMREAD_COLOR)
 
     cv2.imshow("Window 1", original_image)
     cv2.setMouseCallback('Window 1', MouseDetector.click_event)
-
-    # lower_threshold = np.array([0, 0, 170])
-    # higher_threshold = np.array([150, 150, 255])
-    # thresholded_image = cv2.inRange(original_image, lower_threshold, higher_threshold)
-    # cv2.imshow("Window 2", thresholded_image)
-
-    # kernel = np.ones((5, 5))
-    # dilated_image = cv2.dilate(thresholded_image, kernel, iterations=1)
-    # eroded_image = cv2.erode(dilated_image, kernel, iterations=8)
-    # cv2.imshow("Window 3", eroded_image)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     x, y = MouseDetector.get_coordinates()
     MouseDetector.print_coordinates()
     color = original_image[y, x]
     print(color)
+
+    lower_threshold = np.array(list(map(lambda value: utils.clamp(
+        value - config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
+    higher_threshold = np.array(list(map(lambda value: utils.clamp(
+        value + config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
+    
+    threshold_image = cv2.inRange(original_image, lower_threshold, higher_threshold)
+    cv2.imshow("Window 2", thresholded_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,4 @@
+
+
+def clamp(num, min_value, max_value):
+    return max(min(num, max_value), min_value)
