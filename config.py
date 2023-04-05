6a2ba71a4f13afc317007271ee68790bc5912a4d
0starzyk <starzyk@student.agh.edu.pl>
2023-04-05 04:01:25 +0200

    Refactor

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-THRESHOLD_TOLERANCE = 50
-MINIMUM_RGB_VALUE = 0
-MAXIMUM_RGB_VALUE = 255
+from enum import Enum
+
+
+class ThresholdOperations(int, Enum):
+    THRESHOLD_TOLERANCE = 50
+    MINIMUM_RGB_VALUE = 0
+    MAXIMUM_RGB_VALUE = 255
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-import config
+from config import *
 from MouseDetector import MouseDetector
 import utils
 
 
 if __name__ == "__main__":
-    original_image = cv2.imread("tree_modified.jpg", cv2.IMREAD_COLOR)
+    original_image = cv2.imread("traffic_light.jpg", cv2.IMREAD_COLOR)
 
     cv2.imshow("Window 1", original_image)
     cv2.setMouseCallback('Window 1', MouseDetector.click_event)
@@ -18,9 +18,11 @@ if __name__ == "__main__":
     color = original_image[y, x]
 
     lower_threshold = np.array(list(map(lambda value: utils.clamp(
-        value - config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
+        value - ThresholdOperations.THRESHOLD_TOLERANCE, ThresholdOperations.MINIMUM_RGB_VALUE,
+        ThresholdOperations.MAXIMUM_RGB_VALUE), color)))
     higher_threshold = np.array(list(map(lambda value: utils.clamp(
-        value + config.THRESHOLD_TOLERANCE, config.MINIMUM_RGB_VALUE, config.MAXIMUM_RGB_VALUE), color)))
+        value + ThresholdOperations.THRESHOLD_TOLERANCE, ThresholdOperations.MINIMUM_RGB_VALUE,
+        ThresholdOperations.MAXIMUM_RGB_VALUE), color)))
     threshold_image = cv2.inRange(original_image, lower_threshold, higher_threshold)
 
     kernel = np.ones((5, 5))
